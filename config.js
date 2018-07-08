86c13e50a1e637482f417dae39e41b6b7bdb601f
unknown <brankofulurija@gmail.com>
2018-07-08 10:49:31 +0200

    Adding library for storing and editing data

--- config.js
+++ config.js
@@ -3,25 +3,25 @@
  *
  */
 
- var environments = {};
+var environments = {};
 
- // Staging (default) environment
- environments.staging = {
-    'httpPort' : 3000,
-    'httpsPort' : 3001,
-    'envName' : 'staging'
- };
+// Staging (default) environment
+environments.staging = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'staging'
+};
 
- // Production environment
- environments.production = {
-    'httpPort' : 5000,
-    'httpsPort' : 5001,
-    'envName' : 'production'
- }
+// Production environment
+environments.production = {
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: 'production'
+}
 
- // Determine environment
- var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : 'staging';
- var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+// Determine environment
+var currentEnvironment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : 'staging';
+var environmentToExport = typeof (environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
 
- // Export chosen environment
- module.exports = environmentToExport;
+// Export chosen environment
+module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -10,102 +10,103 @@ var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
 var fs = require('fs');
+var _data = require('./lib/data');
 
 // Create HTTP server
-var httpServer = http.createServer(function(req, res) {
-    server(req, res);
+var httpServer = http.createServer(function (req, res) {
+  server(req, res);
 });
 
 // Start HTTP server
-httpServer.listen(config.httpPort, function() {
-    console.log('Server is listening on port ' + config.httpPort + ' in ' + config.envName + ' environment...');
+httpServer.listen(config.httpPort, function () {
+  console.log('Server is listening on port ' + config.httpPort + ' in ' + config.envName + ' environment...');
 });
 
 // Create HTTPS server
 var httpsServerOptions = {
-    'key' : fs.readFileSync('./https/key.pem'),
-    'cert' : fs.readFileSync('./https/cert.pem')
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
 };
-var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-    server(req, res);
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  server(req, res);
 });
 
 // Start HTTPS server
-httpsServer.listen(config.httpsPort, function() {
-    console.log('Server is listening on port ' + config.httpsPort + ' in ' + config.envName + ' environment...');
+httpsServer.listen(config.httpsPort, function () {
+  console.log('Server is listening on port ' + config.httpsPort + ' in ' + config.envName + ' environment...');
 });
 
-var server = function(req, res) {
-    
-    // Get the url and parse it
-    var parsedUrl = url.parse(req.url, true);
-    
-    // Get the path
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-   
-    // Get the query string as object
-    var queryStringObject = parsedUrl.query;
-    
-    // Get the method
-    var method = req.method.toLowerCase();
-    
-    // Get the headers
-    var headers = req.headers;
-    
-    // Get the payload
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-       
-    req.on('data', function(data) {
-        buffer += decoder.write(data);
+var server = function (req, res) {
+
+  // Get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as object
+  var queryStringObject = parsedUrl.query;
+
+  // Get the method
+  var method = req.method.toLowerCase();
+
+  // Get the headers
+  var headers = req.headers;
+
+  // Get the payload
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function () {
+    buffer += decoder.end();
+
+    // Choose handler for request handling
+    var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct data object to send to handler
+    var data = {
+      path: trimmedPath,
+      query: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer
+    }
+
+    // Route request to choosen handler
+    chosenHandler(data, function (statusCode, resPayload) {
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+      resPayload = typeof (resPayload) == 'object' ? resPayload : {};
+
+      payloadString = JSON.stringify(resPayload);
+
+      // Return response
+      res.setHeader('Content-Type', 'application-json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
     });
-    
-    req.on('end', function() {
-        buffer += decoder.end();
-    
-        // Choose handler for request handling
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-    
-        // Construct data object to send to handler
-        var data = {
-            'path' : trimmedPath,
-            'query' : queryStringObject,
-            'method' : method,
-            'headers' : headers,
-            'payload' : buffer
-        }
-    
-        // Route request to choosen handler
-        chosenHandler(data, function(statusCode, resPayload) {
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-             resPayload = typeof(resPayload) == 'object' ? resPayload : {};
-    
-            payloadString = JSON.stringify(resPayload);
-    
-             // Return response
-            res.setHeader('Content-Type', 'application-json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
-        });
-    
-    });    
+
+  });
 }
 
 // Define the handlers
 var handlers = {};
 
 // Ping handler
-handlers.ping = function(data, callback) {
-    callback(200);
+handlers.ping = function (data, callback) {
+  callback(200);
 };
 
 // Not Found handler
-handlers.notFound = function(data, callback) {
-    callback(404);
+handlers.notFound = function (data, callback) {
+  callback(404);
 };
 
 // Define the request router
 var router = {
-    'ping' : handlers.ping
+  'ping': handlers.ping
 };
